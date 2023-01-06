1c148469cd7d8a5cdcfafdb27010c24be5a2e6c0
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-01-06 23:12:14 +0530

    backend commentss

--- router/getCurrentUser.js
+++ router/getCurrentUser.js
@@ -6,7 +6,8 @@ const Authenticate = require('../middlewere/authenticate')
 router.get('/getCurrentUser', Authenticate,(req,res) => {
     
     const {_id, name, email, myCars, myFavouriteCars,requestsToMe, requestsByMe, image} = req.user
-    // console.log(req.user);
+    console.log(req.user);
+    console.log('hello from get current user')
     res.status(201).json({user: {
         _id,
         name,

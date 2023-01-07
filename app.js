157af7251b106111437e6a1e389293e8d740d143
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-01-07 13:48:31 +0530

    correncted path

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(require('./router/updateProfile'))
 app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.get("*", function (_, res) {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html")),
+    res.sendFile(path.join(__dirname, "./client/build/index.html")),
     function (err) {
         if (err) {
             res.status(500).send(err);

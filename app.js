6770e6c16032a611bb6cbb15a9ddb2efae504375
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-01-07 13:43:17 +0530

    added get req in app.js sending index.html

--- app.js
+++ app.js
@@ -46,6 +46,15 @@ app.use(require('./router/postLikeDislike'))
 app.use(require('./router/updateProfile'))
 
 app.use(express.static(path.join(__dirname,"./client/build")))
+
+app.get("*", function (_, res) {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html")),
+    function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    }
+})
 // app.get('/', (req,res) => {
 //     res.send('hello')
 // })

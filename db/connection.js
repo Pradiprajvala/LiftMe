c45bbac741215d25bec9a04f641f3162bb3e3bb0
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-01-09 18:32:13 +0530

    removed backend part

--- db/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const DB = process.env.DATABASE
-
-mongoose.connect(DB).then((res) => {
-    console.log('connection sucess')
-}).catch((err) => console.log(err))

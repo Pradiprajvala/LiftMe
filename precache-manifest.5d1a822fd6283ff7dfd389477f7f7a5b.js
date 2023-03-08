6a512c655239b1121319de7a0e9620e1a4f0e06c
PradipRaj <u20ce053@ced.svnit.ac.in>
2023-03-08 21:25:39 +0530

    Updates

---
+++ precache-manifest.5d1a822fd6283ff7dfd389477f7f7a5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0d98afb50391b822308",
+    "url": "/LiftMe/static/css/main.73b49b53.chunk.css"
+  },
+  {
+    "revision": "a0d98afb50391b822308",
+    "url": "/LiftMe/static/js/main.3329a9a0.chunk.js"
+  },
+  {
+    "revision": "c7b35274a8410f76c940",
+    "url": "/LiftMe/static/js/runtime~main.dda6a739.js"
+  },
+  {
+    "revision": "f4e47fdc4780dc9e4dab",
+    "url": "/LiftMe/static/js/2.2654e66d.chunk.js"
+  },
+  {
+    "revision": "c69b480104a9ffb8ad00",
+    "url": "/LiftMe/static/js/3.f0a16184.chunk.js"
+  },
+  {
+    "revision": "37a9b6f8655f93a0760e82890d7344d2",
+    "url": "/LiftMe/static/media/CarentLogo.37a9b6f8.svg"
+  },
+  {
+    "revision": "88b96f263fa1ab5d8a28b2507ad8d71e",
+    "url": "/LiftMe/static/media/selectCarImage.88b96f26.jpg"
+  },
+  {
+    "revision": "c8063c1a61abbc2e5890397942d2e0ec",
+    "url": "/LiftMe/index.html"
+  }
+];
